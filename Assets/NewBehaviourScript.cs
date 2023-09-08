b4d9fa35481136756e70f2cce3f753f3835cae00
tokwalover666 <91291v@gmail.com>
2023-09-08 13:26:25 +0800

    yey

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using UnityEngine UIElements;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private Renderer rend;
+    public Color[] colors;
+    public Vector3 position;
+    public float speed;
+    public Transform pointA, pointB;
+    float radius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        //transform.position = Vector3.forward;
+    }
+
+    void Update()
+    {
+        //transform.position = Vector3.Lerp(pointA.position, pointB.position , speed * Time.deltaTime);
+        //transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, pointA.position, speed * Time.deltaTime);
+        //int dist = Vector3.Distance(transform.position, pointA.position);
+        float dist
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+    void OnEnable()
+    {
+        //rend.material.color = colors[Random.Range(0, colors.Length)];
+    }
+
+    void OnDisable()
+    {
+
+    }
+    void OnMouseUp()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        //rend.material.color = colors[Random.Range(0, colors.Length)];
+    }
+
+
+}
